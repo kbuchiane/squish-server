be117841df269639fc1ecd1601292e66ca6b8143
kbuchiane <kbuchiane@gmail.com>
2020-04-24 19:03:03 -0400

    progress

--- routes/login.js
+++ routes/login.js
@@ -2,13 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET call from client. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     console.log('login username: ' + req.query.username);
     console.log('login password: ' + req.query.password);
 
     var response = {
         success: true,
-        userData: req.query.username + ' logged in'
+        userData: {
+            username: req.query.username,
+            email: ''
+        }
     };
 
     res.send(response);
--- routes/signup.js
+++ routes/signup.js
@@ -2,14 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET call from client. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     console.log('signup username: ' + req.query.username);
     console.log('signup email: ' + req.query.email);
     console.log('signup password: ' + req.query.password);
 
     var response = {
         success: true,
-        userData: req.query.username + ' logged in'
+        userData: {
+            username: req.query.username,
+            email: req.query.email
+        }
     };
 
     res.send(response);
