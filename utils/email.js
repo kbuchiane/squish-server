c535e4a136e3f47af82d142a936eb9b88af69574
kbuchiane <kbuchiane@gmail.com>
2020-06-02 14:36:20 -0400

    update

--- utils/email.js
+++ utils/email.js
@@ -1,18 +1,19 @@
 const nodemailer = require("nodemailer");
 const appConfig = require("../config/app.config");
+const authConfig = require("../config/auth.config");
 
 exports.sendConfirmation = (emailAddr, confirmId) => {
     return new Promise(function (resolve, reject) {
         let transporter = nodemailer.createTransport({
             service: "gmail",
             auth: {
-                user: process.env.confirm_email_host,
-                pass: process.env.confirm_email_pass
+                user: authConfig.EMAIL_HOST,
+                pass: authConfig.EMAIL_PASS
             }
         });
 
         let mailOptions = {
-            from: process.env.confirm_email_host,
+            from: authConfig.EMAIL_HOST,
             to: emailAddr,
             subject: "Squish Sign Up Confirmation",
             html: "<b style='font-weight:700'>Verification Code (expires in 4 minutes): "
