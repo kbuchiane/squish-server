98f6b7c13e07c540c652751ad183a9955a796117
jnbuchi <jack.buchiane@gmail.com>
2021-01-03 16:40:19 -0500

    updates for like clip handler

---
+++ routes/likeClip.route.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { auth } = require("../middleware");
+const { verifyCredentials } = require("../middleware");
+const likeController = require("../controllers/like.controller");
+const router = express.Router();
+
+router.post("/",
+    [
+        verifyCredentials.checkCredentials,
+        auth.verifyToken
+    ],
+    likeController.likeClip
+);
+
+module.exports = router;
